77e3435576af8997e7f408bf141342d3dd836178
mauricio rojas díaz <freman226@gmail.com>
2024-11-30 21:07:43 -0400

    Version ready for review

--- backend/habragasolina/src/main/java/bo/edu/usfa/gasolina/habragasolina/Entities/User.java
+++ backend/habragasolina/src/main/java/bo/edu/usfa/gasolina/habragasolina/Entities/User.java
@@ -3,7 +3,6 @@ package bo.edu.usfa.gasolina.habragasolina.Entities;
 import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;

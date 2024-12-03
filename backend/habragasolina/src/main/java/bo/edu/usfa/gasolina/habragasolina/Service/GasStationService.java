26b02866973289d81257cb3b00c172961b44ea80
mauricio rojas díaz <freman226@gmail.com>
2024-12-03 15:27:56 -0400

    added update Gas station endpoint

--- backend/habragasolina/src/main/java/bo/edu/usfa/gasolina/habragasolina/Service/GasStationService.java
+++ backend/habragasolina/src/main/java/bo/edu/usfa/gasolina/habragasolina/Service/GasStationService.java
@@ -1,6 +1,10 @@
 package bo.edu.usfa.gasolina.habragasolina.Service;
 
 import bo.edu.usfa.gasolina.habragasolina.Repository.GasStationRepository;
+import bo.edu.usfa.gasolina.habragasolina.Entities.GasStation;
+
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +16,18 @@ public class GasStationService {
         this.gasStationRepository = gasStationRepository;
     }
 
+    public GasStation updateGasStation(Integer id, GasStation updatedGasStation) {
+        Optional<GasStation> existingGasStation = gasStationRepository.findById(id);
+        if (existingGasStation.isPresent()) {
+            GasStation gasStation = existingGasStation.get();
+            gasStation.setName(updatedGasStation.getName());
+            gasStation.setLocation(updatedGasStation.getLocation());
+            return gasStationRepository.save(gasStation);
+        } else {
+            throw new RuntimeException("Gas station not found, id " + id);
+        }
+    }
+
     public void deleteGasStation(Integer id) {
         if (gasStationRepository.existsById(id)) {
             gasStationRepository.deleteById(id);
@@ -19,5 +35,6 @@ public class GasStationService {
             throw new RuntimeException("Gas station not found, id: " + id);
         }
     }
+    
 }
 

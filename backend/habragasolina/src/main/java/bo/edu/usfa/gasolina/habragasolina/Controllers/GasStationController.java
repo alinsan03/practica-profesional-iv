bccb830d8962051c43bb8a42306bff24a5f1133f
Mosipro1 <mosiremit@gmail.com>
2024-12-03 18:19:40 -0400

    get-gas-station endpoint is ready for review

--- backend/habragasolina/src/main/java/bo/edu/usfa/gasolina/habragasolina/Controllers/GasStationController.java
+++ backend/habragasolina/src/main/java/bo/edu/usfa/gasolina/habragasolina/Controllers/GasStationController.java
@@ -1,5 +1,6 @@
 package bo.edu.usfa.gasolina.habragasolina.Controllers;
 
+import bo.edu.usfa.gasolina.habragasolina.Entities.GasStation;
 import bo.edu.usfa.gasolina.habragasolina.Service.GasStationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,5 +24,15 @@ public class GasStationController {
             return ResponseEntity.status(404).body(e.getMessage());
         }
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GasStation> getGasStation(@PathVariable Integer id) {
+        try {
+            GasStation gasStation = gasStationService.getGasStation(id);
+            return ResponseEntity.ok(gasStation);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(404).build();
+        }
+    }    
 }
 
--- backend/habragasolina/src/main/java/bo/edu/usfa/gasolina/habragasolina/Service/GasStationService.java
+++ backend/habragasolina/src/main/java/bo/edu/usfa/gasolina/habragasolina/Service/GasStationService.java
@@ -1,7 +1,10 @@
 package bo.edu.usfa.gasolina.habragasolina.Service;
 
+import bo.edu.usfa.gasolina.habragasolina.Entities.GasStation;
 import bo.edu.usfa.gasolina.habragasolina.Repository.GasStationRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class GasStationService {
@@ -18,6 +21,12 @@ public class GasStationService {
         } else {
             throw new RuntimeException("Gas station not found, id: " + id);
         }
+    }
+
+    public GasStation getGasStation(Integer id){
+        Optional<GasStation> gasStation = gasStationRepository.findById(id);
+        return gasStation.orElseThrow(() -> 
+            new RuntimeException("Gas station not found, id: " + id));
     }
 }
 

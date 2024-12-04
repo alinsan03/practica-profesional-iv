b0d5cb823f671835998a18f4f8065855cc8dd629
Lucia Lopez Videla <lalopezvidela@gmail.com>
2024-12-03 23:01:30 -0400

    Add changes of files (controller and entity)

---
+++ backend/habragasolina/src/main/java/bo/edu/usfa/gasolina/habragasolina/entity/User.java
@@ -0,0 +1,57 @@
+package bo.edu.usfa.gasolina.habragasolina.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user") 
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "active", nullable = false)
+    private boolean active = true; 
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}

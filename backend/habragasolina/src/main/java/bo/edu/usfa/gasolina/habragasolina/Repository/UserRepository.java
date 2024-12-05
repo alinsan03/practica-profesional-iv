acca99d87aebd583170d6e1cc63cd80c5664780c
Jhordan <feamaka123@gmail.com>
2024-12-05 12:45:23 -0600

    Add the update for validations

--- backend/habragasolina/src/main/java/bo/edu/usfa/gasolina/habragasolina/Repository/UserRepository.java
+++ backend/habragasolina/src/main/java/bo/edu/usfa/gasolina/habragasolina/Repository/UserRepository.java
@@ -2,6 +2,8 @@ package bo.edu.usfa.gasolina.habragasolina.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Query;
 
 import bo.edu.usfa.gasolina.habragasolina.Entities.User;
 
@@ -9,4 +11,8 @@ import bo.edu.usfa.gasolina.habragasolina.Entities.User;
 public interface UserRepository extends JpaRepository <User, Long>{
     boolean existsByUsername(String username);
     User findByUsername(String username);
+
+    @Query("SELECT COUNT(u) > 0 FROM User u WHERE u.id_gas_station = :gasStationId")
+    boolean existsByGasStationId(@Param("gasStationId") Integer gasStationId);
+
 }

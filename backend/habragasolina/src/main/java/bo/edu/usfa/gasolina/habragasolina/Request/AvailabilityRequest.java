526309b68bc82eaaff0ed5b5ebb06ab119a786ed
miguel juchasara <luismigueljuchasaracruz08@gmail.com>
2024-12-09 22:09:13 -0400

    updateavailability

---
+++ backend/habragasolina/src/main/java/bo/edu/usfa/gasolina/habragasolina/Request/AvailabilityRequest.java
@@ -0,0 +1,24 @@
+package bo.edu.usfa.gasolina.habragasolina.Request;
+
+public class AvailabilityRequest {
+
+    private Integer idFuelType;
+        private Integer idStatus;
+
+        public Integer getIdFuelType() {
+            return idFuelType;
+        }
+
+        public void setIdFuelType(Integer idFuelType) {
+            this.idFuelType = idFuelType;
+        }
+
+        public Integer getIdStatus() {
+            return idStatus;
+        }
+
+        public void setIdStatus(Integer idStatus) {
+            this.idStatus = idStatus;
+        }
+    
+}

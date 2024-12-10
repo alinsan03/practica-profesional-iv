795283566c15d6efc0b4a2018b685b2249d4c29d
Alejandro Javier Sanchez Roa <alejandro.j.sanchez@bairesdev.com>
2024-12-10 09:29:49 -0400

    Add header and footer

--- frontend/habragasolina-web/resources/views/main.blade.php
+++ frontend/habragasolina-web/resources/views/main.blade.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tabla de Datos</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
+@include('components.header')
 <body>
     <div class="container mt-5">
         <h1 class="mb-4">Habrá Gasolina</h1>
@@ -39,4 +32,4 @@
         </table>
     </div>
 </body>
-</html>
+@include('components.footer')

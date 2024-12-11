b9beb619ee443a69181ace5a8570ae6d05eb73b3
Franco Castro <frankensteincastro78@gmail.com>
2024-12-11 10:58:30 -0400

    Buttons centered

--- frontend/habragasolina-web/resources/views/admin-page.blade.php
+++ frontend/habragasolina-web/resources/views/admin-page.blade.php
@@ -5,31 +5,45 @@
 		<br>
 		<br>
         <div class="label-group">
-            <label for="label1">User:</label>
+            <label for="label1">Usuario:</label>
 			<label for="label1">Test</label>
         </div>
         <div class="label-group">
-            <label for="label2">Station:</label>
+            <label for="label2">Estación de Gasolina:</label>
             <label for="label">Test</label>
         </div>
         <div class="label-group">
-            <label for="dropdown">Fuel type:</label>
+            <label for="dropdown">Tipo de Gasolina:</label>
             <select id="dropdown">
-                <option value="">Select an option:</option>
+                <option value="">Selecciona una opción:</option>
                 @foreach ($data as $fuel)
                 <option value="option{{$fuel['id']}}">{{$fuel['name']}}</option>
                 @endforeach
             </select>
         </div>
-        <div>
-            <button class="available">Disponible</button>
-        </div>
+            <div>
+                <center>
+                <table>
+                    <tr>
+                        <td>
+                <div>
+                    <button class="available">Disponible</button>
+                </div>
+                        </td>
+                        <td>
         <div>
             <button  class="notavailable">No disponible</button>
         </div>
+        </td>
+        <td>
         <div>
             <button class="notsale">No se vende</button>
         </div>
+        </td>
+        </tr>
+        </table>
+        </center>
+    </div>
     </div>
 </body>
 @include('components.footer')
